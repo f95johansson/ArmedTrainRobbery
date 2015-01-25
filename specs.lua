e4acc02f7c0c35d3bd36245df8f19192c9abc1be
syltman <mcoquand@gmail.com>
2015-01-25 11:14:58 +0100

    Added stuff to specs

--- specs.lua
+++ specs.lua
@@ -6,9 +6,17 @@ return {
     },
     position = {
         agent = {120, 200},
+        trashcan = {440,170},
+        waitress = {424,112},
+        closet = {54,256},
+        driver = {280,256},
     },
     level = {
         agent = 1,
+        waitress = 1,
+        trashcan = 1,
+        closet = 2,
+        driver = 3,
 
     }
 }
